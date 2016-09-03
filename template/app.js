c6980c50325d507650cca3542aee0ce836867e1b
chandankrishnan <chandan.mandre10@gmail.com>
2016-09-03 18:53:25 +0530

    socket.io

--- template/app.js
+++ template/app.js
@@ -1,6 +1,5 @@
 // MODIFY THIS TO THE APPROPRIATE URL IF IT IS NOT BEING RUN LOCALLY
-var socket = io.connect('http://localhost:8088');
-
+var socket = io.connect('http://localhost:8089');
 var canvas = document.getElementById('canvas-video');
 var context = canvas.getContext('2d');
 var img = new Image();
@@ -10,7 +9,6 @@ context.fillStyle = '#333';
 context.fillText('Loading...', canvas.width/2-30, canvas.height/3);
 
 socket.on('frame', function (data) {
-	console.log("data",data);
   // Reference: http://stackoverflow.com/questions/24107378/socket-io-began-to-support-binary-stream-from-1-0-is-there-a-complete-example-e/24124966#24124966
   var uint8Arr = new Uint8Array(data.buffer);
   var str = String.fromCharCode.apply(null, uint8Arr);
@@ -20,5 +18,4 @@ socket.on('frame', function (data) {
     context.drawImage(this, 0, 0, canvas.width, canvas.height);
   };
   img.src = 'data:image/png;base64,' + base64String;
-  socket.emit('data',data)
 });
